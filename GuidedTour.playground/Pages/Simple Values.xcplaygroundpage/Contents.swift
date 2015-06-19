85033fe1acf34fd0bf0398a3f14980ac56cb4a5b
Chris Clark <christophomos@gmail.com>
2015-06-18 21:21:28 -0700

    - Marked "Simple Values" passed

--- GuidedTour.playground/Pages/Simple Values.xcplaygroundpage/Contents.swift
+++ GuidedTour.playground/Pages/Simple Values.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@
 //:
 //: Tradition suggests that the first program in a new language should print the words “Hello, world!” on the screen. In Swift, this can be done in a single line:
 //:
+"CHRIS CLARK MARKS: PASSED"
+
 print("Hello, world!")
 print("value")
 
