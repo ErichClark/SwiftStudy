6c81f9d0c9086f46605791ab105696b93b8f1c0a
ErichClark <erich.clark@gmail.com>
2015-06-29 23:17:00 -0400

    Finished, with much help.

--- GuidedTour.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ GuidedTour.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -23,23 +23,21 @@ possibleInteger = .Some(100)
 
 //: Use `where` after the type name to specify a list of requirements—for example, to require the type to implement a protocol, to require two types to be the same, or to require a class to have a particular superclass.
 //:
-// What  I want to do is 
+// Gotta be honest, this one was beyond me. I grabbed some code from the internet, which did not work, but I fixed it. they used "Sequence" and "GeneratorType", instead of "SequenceType" and "Generator" I do understand what it"s doing, but yeah, never would have gotten there without the boost.
 
-func anyCommonElements <T, U where T: SequenceType, U: SequenceType, T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, _ rhs: U) -> Array<T.Generator.Element> {
-    var inCommon = Array<T.Generator.Element>()
+func anyCommonElements <T, U where T: SequenceType, U: SequenceType, T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, rhs: U) -> [T.Generator.Element] {
+    var commonElements : [T.Generator.Element] = []
     for lhsItem in lhs {
         for rhsItem in rhs {
             if lhsItem == rhsItem {
-                inCommon.append(rhsitem)
+                commonElements.append(lhsItem)
             }
         }
-        return inCommon
     }
-
+   return commonElements
 }
-
-anyCommonElements([1, 2, 3], [3])
-
+ 
+anyCommonElements([1, 2, 3], rhs: [3]) // 3
 //: > **Experiment**:
 //: > Modify the `anyCommonElements(_:_:)` function to make a function that returns an array of the elements that any two sequences have in common.
 //:
